d452bd9db5694a866ea7038d323acd727b620487
Druganov <77912396+Druganov1@users.noreply.github.com>
2023-12-24 00:36:31 +0100

    Update DiscordUser.php
    add dienstnummer
    
    Signed-off-by: Druganov <77912396+Druganov1@users.noreply.github.com>

--- src/Models/DiscordUser.php
+++ src/Models/DiscordUser.php
@@ -9,5 +9,5 @@ class DiscordUser extends Model
     /**
      * The attributes that are mass assignable.
      */
-    protected $fillable = ['id', 'username', 'discriminator', 'global_name', 'email', 'avatar', 'verified', 'banner', 'accent_color', 'public_flags', 'flags', 'locale', 'premium_type', 'mfa_enabled'];
+    protected $fillable = ['id', 'username', 'discriminator', 'global_name', 'email', 'avatar', 'verified', 'banner', 'accent_color', 'public_flags', 'flags', 'locale', 'premium_type', 'mfa_enabled', 'dienstnummer'];
 }
